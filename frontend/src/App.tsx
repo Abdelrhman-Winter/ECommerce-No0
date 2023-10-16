4de820dbd3ee851e54e3e9471544bccaf29b47de
winter <wwwez1562@gmail.com>
2023-10-17 02:01:06 +0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>Amazon winter</header>
+      <main></main>
+      <footer>all right reserved</footer>
+    </div>
+  )
+}
+
+export default App
